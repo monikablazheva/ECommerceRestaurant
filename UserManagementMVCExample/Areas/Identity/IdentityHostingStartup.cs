ab4f4fa3a52e1cabf502d5697996306ab5eb2cad
monikablazheva <monikablazheva_e18@schoolmath.eu>
2022-10-03 09:12:30 +0300

    Add project files.

---
+++ UserManagementMVCExample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManagementMVCExample.Data;
+
+[assembly: HostingStartup(typeof(UserManagementMVCExample.Areas.Identity.IdentityHostingStartup))]
+namespace UserManagementMVCExample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ UserManagementMVCExample/Enums/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementMVCExample.Enums
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
