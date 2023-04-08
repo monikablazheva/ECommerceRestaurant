ab070578d3a2a3d0ab6237a800bbdee9a91e3bcb
monikablazheva <monikablazheva_e18@schoolmath.eu>
2023-04-08 13:01:25 +0300

    New menu design

--- UserManagementMVCExample/wwwroot/js/site.js
+++ UserManagementMVCExample/wwwroot/js/site.js
@@ -2,17 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-/*$(document).ready(function () {
-    //Click for plus
-    $("#span_plus").click(function () {       
-        $('.count').val(parseInt($('.count').val()) + 1);
-    });
-    //Click for minus
-    $("#span_minus").click(function () {        
-        $('.count').val(parseInt($('.count').val()) - 1);
-        if ($('.count').val() == 0) {
-            $('.count').val(1);
-        }
-    });
-});*/
